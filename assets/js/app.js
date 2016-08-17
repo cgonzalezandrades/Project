6a3d10baa120d1a458dc8a94d667e7bbcff00166
Carlos Gonzalez <carlosgonzalez@carloss-mbp.local.valenciacc.edu>
2016-08-17 19:35:03 -0400

    initial push

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+<script>
+        var geocoder;
+        var map;
+
+        function initialize() {
+
+            geocoder = new google.maps.Geocoder();
+            var latlng = new google.maps.LatLng(-34.397, 150.644);
+            var mapOptions = {
+                zoom: 10,
+                center: latlng
+            }
+            map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+        }
+
+
+        function codeAddress() {
+
+            var address = document.getElementById('address').value;
+            geocoder.geocode({
+                'address': address
+            }, function (results, status) {
+                console.log(results[0].geometry.location);
+
+
+                if (status == 'OK') {
+                    map.setCenter(results[0].geometry.location);
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: results[0].geometry.location
+                    });
+
+                } else {
+                    alert('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+        }
+    </script>

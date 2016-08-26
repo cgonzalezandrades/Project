0f589f1010e9ac6e35579cf05672d2785b366880
sergsal <sergio.saldarriaga88@gmail.com>
2016-08-26 14:27:08 -0400

    Adds geolocation and refinements to apis

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,83 +33,88 @@ $("#get-product-text").on('click', function () {
 
 //Function to display the map for the click function
 function initMap() {
+    jQuery.post( "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDCa1LUe1vOczX1hO_iGYgyo8p_jYuGOPU", function(success) {
+        apiGeolocationSuccess({coords: {latitude: success.location.lat, longitude: success.location.lng}});
+        })
+    .fail(function(err) {
+        alert("API Geolocation error! \n\n"+err);
+    });
+  //   var latlng = new google.maps.LatLng(28.495, -81.400);
+  //   var mapOptions = {
+  //       zoom: 10,
+  //       center: latlng,
+  //   };
+  //   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  //   var centerMarker = new google.maps.Marker({
+  //           map: map,});
+  //   console.log(centerMarker)
+  //   // Try HTML5 geolocation.
+  //     if (navigator.geolocation) {
+  //   navigator.geolocation.getCurrentPosition(function(position) {
+  //     var pos = {
+  //       lat: position.coords.latitude,
+  //       lng: position.coords.longitude
+  //     };
 
-    var latlng = new google.maps.LatLng(28.495, -81.400);
-    var mapOptions = {
-        zoom: 10,
-        center: latlng,
-    };
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    var centerMarker = new google.maps.Marker({
-            map: map,});
-    console.log(centerMarker)
-    // Try HTML5 geolocation.
-      if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-      center.setPosition(pos);
+  //     centerMarker.setPosition(pos);
       
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, centerMarker, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, centerMarker, map.getCenter());
-  }
+  //     map.setCenter(pos);
+  //   }, function() {
+  //     handleLocationError(true, centerMarker, map.getCenter());
+  //   });
+  // } else {
+  //   // Browser doesn't support Geolocation
+  //   handleLocationError(false, centerMarker, map.getCenter());
+  // }
     
-    database.ref().on("child_added", function (snapshot) {
-        //content for the info window when clicking on marker
-        var contentString = snapshot.val().ingredient;
-        //format of the image that replaces the standard google maps marker
-        var image = {url: (snapshot.val().imgURL),
-            scaledSize: new google.maps.Size(40,40),
-            origin: new google.maps.Point(0,0),
-            anchor: new google.maps.Point(0, 0),
-            };
-        //item location from firebase
-        var itemLocation = JSON.parse(snapshot.val().location);
-        var locationForDiv = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + itemLocation.lat + "," + itemLocation.lng + "&key=AIzaSyAdC7G5MC9G8uxPUYabGBD93xL-lyzRu54"
+    // database.ref().on("child_added", function (snapshot) {
+    //     //content for the info window when clicking on marker
+    //     var contentString = snapshot.val().ingredient;
+    //     //format of the image that replaces the standard google maps marker
+    //     var image = {url: (snapshot.val().imgURL),
+    //         scaledSize: new google.maps.Size(40,40),
+    //         origin: new google.maps.Point(0,0),
+    //         anchor: new google.maps.Point(0, 0),
+    //         };
+    //     //item location from firebase
+    //     var itemLocation = JSON.parse(snapshot.val().location);
+    //     var locationForDiv = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + itemLocation.lat + "," + itemLocation.lng + "&key=AIzaSyAdC7G5MC9G8uxPUYabGBD93xL-lyzRu54"
 
-        //Commenting out below, but leaving in code just in case we want to print ingredient information on the page.
-        // $.ajax({
-        //         url: locationForDiv,
-        //         method: 'GET'
-        //     })
-        //     .done(function (response) {
-        //         // var name = snapshot.val().name;
-        //         console.log(itemLocation);
-        //         // console.log(name);
-        //         console.log(contentString);
-        //         var itemLoc = response.results[0].address_components[2].short_name;
-        //         console.log(itemLoc);
-        //         var itemDiv = $('<div/>');
-        //         itemDiv.addClass('userInfo');
-        //         itemDiv.append("<span>" + name + "</span>");
-        //         itemDiv.append("<span>" + itemLoc + "</span>");
-        //         itemDiv.append("<span>" + contentString + "</span>");
-        //         $('#userInfo').append(itemDiv);
-        //     });
-        //creating info windo
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString,
-        });
-        //place marker on the map
-        var marker = new google.maps.Marker({
-            position: itemLocation,
-            map: map,
-            icon: image,
-            title: 'Ingredients'
-        });
-        //open info window when clicking on the marker
-        marker.addListener('click', function () {
-            infowindow.open(map, marker);
-        });
-    });
+    //     //Commenting out below, but leaving in code just in case we want to print ingredient information on the page.
+    //     // $.ajax({
+    //     //         url: locationForDiv,
+    //     //         method: 'GET'
+    //     //     })
+    //     //     .done(function (response) {
+    //     //         // var name = snapshot.val().name;
+    //     //         console.log(itemLocation);
+    //     //         // console.log(name);
+    //     //         console.log(contentString);
+    //     //         var itemLoc = response.results[0].address_components[2].short_name;
+    //     //         console.log(itemLoc);
+    //     //         var itemDiv = $('<div/>');
+    //     //         itemDiv.addClass('userInfo');
+    //     //         itemDiv.append("<span>" + name + "</span>");
+    //     //         itemDiv.append("<span>" + itemLoc + "</span>");
+    //     //         itemDiv.append("<span>" + contentString + "</span>");
+    //     //         $('#userInfo').append(itemDiv);
+    //     //     });
+    //     //creating info windo
+    //     var infowindow = new google.maps.InfoWindow({
+    //         content: contentString,
+    //     });
+    //     //place marker on the map
+    //     var marker = new google.maps.Marker({
+    //         position: itemLocation,
+    //         map: map,
+    //         icon: image,
+    //         title: 'Ingredients'
+    //     });
+    //     //open info window when clicking on the marker
+    //     marker.addListener('click', function () {
+    //         infowindow.open(map, marker);
+    //     });
+    // });
 }; //end of init map
 
 //function to record fields when the button is clicked
@@ -274,4 +279,78 @@ function handleLocationError(browserHasGeolocation, centerMarker, pos) {
   alert(browserHasGeolocation ?
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
-}
+};
+// var apiGeolocationSuccess = function(position) {
+//     alert("API geolocation success!\n\nlat = " + position.coords.latitude + "\nlng = " + position.coords.longitude);
+// };
+
+// var tryAPIGeolocation = function() {
+
+// };
+// tryAPIGeolocation()
+apiGeolocationSuccess = function(position) {
+    var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+        }
+    var latlng = new google.maps.LatLng(28.495, -81.400);
+    var mapOptions = {
+        zoom: 10,
+        center: latlng,
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var centerMarker = new google.maps.Marker({
+            map: map,});
+    centerMarker.setPosition(pos);
+      
+    map.setCenter(pos);
+
+    database.ref().on("child_added", function (snapshot) {
+        //content for the info window when clicking on marker
+        var contentString = snapshot.val().ingredient;
+        //format of the image that replaces the standard google maps marker
+        var image = {url: (snapshot.val().imgURL),
+            scaledSize: new google.maps.Size(40,40),
+            origin: new google.maps.Point(0,0),
+            anchor: new google.maps.Point(0, 0),
+            };
+        //item location from firebase
+        var itemLocation = JSON.parse(snapshot.val().location);
+        var locationForDiv = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + itemLocation.lat + "," + itemLocation.lng + "&key=AIzaSyAdC7G5MC9G8uxPUYabGBD93xL-lyzRu54"
+
+        //Commenting out below, but leaving in code just in case we want to print ingredient information on the page.
+        // $.ajax({
+        //         url: locationForDiv,
+        //         method: 'GET'
+        //     })
+        //     .done(function (response) {
+        //         // var name = snapshot.val().name;
+        //         console.log(itemLocation);
+        //         // console.log(name);
+        //         console.log(contentString);
+        //         var itemLoc = response.results[0].address_components[2].short_name;
+        //         console.log(itemLoc);
+        //         var itemDiv = $('<div/>');
+        //         itemDiv.addClass('userInfo');
+        //         itemDiv.append("<span>" + name + "</span>");
+        //         itemDiv.append("<span>" + itemLoc + "</span>");
+        //         itemDiv.append("<span>" + contentString + "</span>");
+        //         $('#userInfo').append(itemDiv);
+        //     });
+        //creating info windo
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+        });
+        //place marker on the map
+        var marker = new google.maps.Marker({
+            position: itemLocation,
+            map: map,
+            icon: image,
+            title: 'Ingredients'
+        });
+        //open info window when clicking on the marker
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });
+    });
+};
